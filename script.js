912d0159345d4496ca022d8aa54c371e741f3212
shokouhSkr <shokouhskr@gmail.com>
2022-06-19 20:57:35 +0430

    functions calling another functions

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+// function calling other functions
+const calAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearUntilRetirement = function (birthYear) {
+  const age = calAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    return retirement;
+  } else {
+    return -1;
+  }
+};
+
+console.log(yearUntilRetirement(1991, "jonas"));
+console.log(yearUntilRetirement(1950, "jason"));
