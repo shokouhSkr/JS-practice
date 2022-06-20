7240a0f27ecd63c86c7472ccfac53d2b63e80911
shokouhSkr <shokouhskr@gmail.com>
2022-06-20 15:44:16 +0430

    calculate tips with array

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 "use strict";
 
-// function calling other functions
-const calAge = function (birthYear) {
-  return 2037 - birthYear;
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 };
 
-const yearUntilRetirement = function (birthYear) {
-  const age = calAge(birthYear);
-  const retirement = 65 - age;
+const bills = [125, 555, 44];
 
-  if (retirement > 0) {
-    return retirement;
-  } else {
-    return -1;
-  }
-};
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 
-console.log(yearUntilRetirement(1991, "jonas"));
-console.log(yearUntilRetirement(1950, "jason"));
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(tips, total);
