932661df896feb62a2b3fc43797838986a840686
shokouhSkr <shokouhskr@gmail.com>
2022-06-20 16:47:31 +0430

    Dot vs bracket notation

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 "use strict";
 
-const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// Dot vs bracket notaition
+const jonas = {
+  firstName: "jonas",
+  lastName: "askari",
+  friends: ["ali", "hasan", "hossein"],
 };
 
-const bills = [125, 555, 44];
+const information = `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is ${jonas.friends[0]}`;
 
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-
-const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
-
-console.log(tips, total);
+console.log(information);
