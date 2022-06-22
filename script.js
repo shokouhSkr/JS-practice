355776203677cbae31a327e7fcba1e9deaa88068
shokouhSkr <shokouhskr@gmail.com>
2022-06-22 13:42:11 +0430

    turning array into string by "join" method

--- script.js
+++ script.js
@@ -1,33 +1,22 @@
 "use strict";
 
-// Looping Arrays, continue and break
-const jonas = ["jonas", "askari", 2037 - 1990, "teacher", true];
+const arr = [17, 21, 23];
 
-console.log("---only strings---");
-
-for (let i = 0; i < jonas.length; i++) {
-  if (typeof jonas[i] !== "string") continue;
-  console.log(jonas[i], typeof jonas[i]);
-}
-
-// Looping backwards
-for (let i = jonas.length - 1; i >= 0; i--) {
-  console.log(i, jonas[i]);
-}
-
-// Loops in loop
-for (let i = 1; i < 4; i++) {
-  console.log(`---------Starting exercise ${i}---------`);
-
-  for (let j = 1; j < 6; j++) {
-    console.log(`Lifting weight repetition ${j} of exercise ${i}`);
+const newArr = [];
+const printForecast = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(`${arr[i]}°C in ${i + 1} days`);
   }
-}
 
-// White loop
-let dice = Math.floor(Math.random() * 6) + 1;
+  return newArr;
+};
 
-while (dice !== 6) {
-  console.log(`You rolled ${dice}`);
-  dice = Math.floor(Math.random() * 6) + 1;
-}
+const result = printForecast(arr);
+
+// console.log(printForecast(arr)); [
+//   "...12°C in 1 days",
+//   "...5°C in 2 days",
+//   "...-5°C in 3 days",
+// ];
+
+console.log(`... ${result.join(" ... ")} ...`);
