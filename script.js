802be09b1b777e7f4fd44112f4e42fca92c90407
shokouhSkr <shokouhskr@gmail.com>
2022-07-02 00:47:39 +0430

    this keyword in practice

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 "use strict";
 
-console.log(me); // undefined
-console.log(job); // ReferenceError: cannot accsess 'job' before initailizstion
-console.log(age); // nothing!
+// "this" keyword in practice
 
-var me = "jonas";
-let job = "teacher";
-const age = 1991;
+// 1.
+console.log(this); // window
+
+// 2. regular function
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this); // undefined
+};
+calcAge(1990);
+
+// 3. arrow function
+const calcAgeArrow = (birthYear) => {
+  console.log(2037 - birthYear);
+  console.log(this); // window
+};
+calcAgeArrow();
+
+// 4.
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this); // {year: 1991, calcAge: ƒ}
+    console.log(2037 - this.year); // 46
+  },
+};
+jonas.calcAge(1980);
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge(); // 20
