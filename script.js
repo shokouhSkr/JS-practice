7fe57a4f6a383439f390bb9819091bb02d5b9def
shokouhSkr <shokouhskr@gmail.com>
2022-07-01 22:06:21 +0430

    hoisting

--- script.js
+++ script.js
@@ -1,32 +1,9 @@
 "use strict";
 
-// scoping in practice
+console.log(me); // undefined
+console.log(job); // ReferenceError: cannot accsess 'job' before initailizstion
+console.log(age); // nothing!
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
-
-  function printAge() {
-    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-    console.log(output);
-
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      output = "NEW OUTPUT";
-      var millenial = true;
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
-      console.log(millenial);
-    }
-
-    console.log(output);
-    console.log(str);
-  }
-
-  printAge();
-
-  return age;
-}
-
-const firstName = "John"; // global scope
-calcAge(1991);
-console.log(age); // ReferenceError
-printAge(); // dosn't know it
+var me = "jonas";
+let job = "teacher";
+const age = 1991;
