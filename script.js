87a4012b4258d172a708ba41e96ab9ed2f4f72fe
shokouhSkr <shokouhskr@gmail.com>
2022-07-09 01:04:04 +0430

    destructing object

--- script.js
+++ script.js
@@ -1,53 +1,101 @@
 "use strict";
 
-// destructing arrays
+// destructing object
 
-const arr = [2, 3, 4];
-const [a, b, c] = arr;
-console.log(c, a, b); // 4, 2, 3
-
-/**********************************************/
 // Example:
-
 const restaurant = {
   name: "Classico Italiano",
   location: "Via Angelo Tavanti 23, Firenze, Italy",
   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
+
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  // orderDelivery: function (obj) {
+  //   console.log(obj);
+  // },
+
+  // default values for times when there is nothing in that position
+  orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = "20:00", address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
 };
 
-// const [first, second] = restaurant.categories;
-// console.log(first, second); // Italian Pizzeria
+const { name, openingHours, categories } = restaurant;
 
-const [first, , third] = restaurant.categories;
-console.log(first, third); // Italian Vegetarian
+console.log(name, openingHours, categories);
+// Classico Italiano {thu: {…}, fri: {…}, sat: {…}}fri: {open: 11, close: 23}sat: {open: 0, close: 24}thu: {open:12, close: 22}[[Prototype]]: Object (4) ['Italian', 'Pizzeria', 'Vegetarian', 'Organic']
 
-/**********************************************/
-// how to switch the place of "Italian" and "vegeterian":
+const { name: restaurantName, categories: tags } = restaurant;
+console.log(restaurantName, tags); // Classico Italiano (4) ['Italian', 'Pizzeria', 'Vegetarian', 'Organic']
 
-let [main, , secondary] = restaurant.categories;
-console.log(main, secondary); // Italian Vegetarian
+console.log(restaurant);
+// {name: 'Classico Italiano', location: 'Via Angelo Tavanti 23, Firenze, Italy', categories: Array(4), starterMenu: Array(4), mainMenu: Array(3), …}
 
-const temp = main;
-main = secondary;
-secondary = temp;
-console.log(main, secondary); // Vegetarian Italian
+/**********************************************************************/
+// default values:
 
-// recieve 2 return values from a function
-const [starter, mainCourse] = restaurant.order(2, 0);
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+// [] (4) ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad']
 
-console.log(starter, mainCourse); // Garlic Bread Pizza
-/**********************************************/
-// destructive nested arrays:
+/**********************************************************************/
+// mutating variables:
 
-const nested = [2, 3, [4, 5]];
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
 
-// const [i, , j] = nested;
-// console.log(i, j); // 2, [4, 5]
+console.log(a, b); // 23 7
 
-const [i, , [j, k]] = nested;
-console.log(i, j, k); // 2, 4, 5
+/**********************************************************************/
+// nested objects:
+
+const { fri } = openingHours;
+console.log(fri); // {open: 11, close: 23}
+
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close); // 11 23
+
+const {
+  fri: { close: c, open: o },
+} = openingHours;
+console.log(c, o); // 23 11
+
+/**********************************************************************/
+restaurant.orderDelivery({
+  time: "23:30",
+  address: "Via del Sole, 21",
+  mainIndex: 2,
+  starterIndex: 2,
+});
+// Order received! Garlic Bread and Risotto will be delivered to Via del Sole, 21 at 23:30
+
+restaurant.orderDelivery({
+  mainIndex: 2,
+  starterIndex: 2,
+});
+// Order received! Garlic Bread and Risotto will be delivered to undefined at 20:00
