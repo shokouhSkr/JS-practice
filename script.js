40b767eba7f5d4de8b58dd5f6f6e8cb6ac1688cf
shokouhSkr <shokouhskr@gmail.com>
2022-07-13 23:14:02 +0430

    data structing challange - part2

--- script.js
+++ script.js
@@ -1,42 +1,118 @@
 "use strict";
 
-// Looping Objects: Object Keys, Values, and Entries
+// Coding Challenge #2
 
-const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-const openingHours = {
-  [weekdays[3]]: {
-    open: 12,
-    close: 22,
-  },
-  [weekdays[4]]: {
-    open: 11,
-    close: 23,
-  },
-  [weekdays[5]]: {
-    open: 0, // Open 24 hours
-    close: 24,
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
   },
 };
 
-// Property NAMES
-const properties = Object.keys(openingHours);
-console.log(properties);
+// 1.
+const players = game.scored;
 
-let openStr = `We are open on ${properties.length} days: `;
-for (const day of properties) {
-  openStr += `${day}, `;
+for (const [i, player] of players.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
 }
-console.log(openStr);
 
-// Property VALUES
-const values = Object.values(openingHours);
-console.log(values);
+// for (let i = 0; i < players.length; i++) {
+//   console.log(`Goal ${i + 1}: ${players[i]}`);
+// }
 
-// Entire object
-const entries = Object.entries(openingHours);
-console.log(entries);
+/**********************************************/
+// 2.
 
-// [key, value]
-for (const [day, { open, close }] of entries) {
-  console.log(`On ${day} we open at ${open} and close at ${close}`);
+const values = Object.values(game.odds);
+
+let odds = 0;
+for (const odd of values) {
+  odds += odd;
 }
+console.log(odds / values.length);
+
+/**********************************************/
+// 3.
+
+const entries = Object.entries(game.odds);
+
+for (const [teamName, score] of entries) {
+  console.log(teamName);
+  // const teamStr = teamName === "x" ? "draw" : `victory ${game[teamName]}`;
+  // console.log(`Odd of ${teamStr}: ${score}`);
+}
+
+// BONUS: Create an object called 'scorers' which contains the names of the players who scored, as properties, and the number of goals as the value. In this game, it will look like this:
+//       {
+//         Gnarby: 1,
+//         Hummels: 1,
+//         Lewandowski: 2
+//       }
+
+/**********************************************/
+// bonus.
+
+// So the solution is to loop over the array, and add the array elements as object properties, and then increase the count as we encounter a new occurence of a certain element
+
+const scorers = {};
+
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+
+console.log(scorers);
