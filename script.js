7ff296ff658ae776c6892a5e797a165f82dd2039
shokouhSkr <shokouhskr@gmail.com>
2022-07-14 19:53:31 +0430

    Sets

--- script.js
+++ script.js
@@ -1,118 +1,31 @@
 "use strict";
 
-// Coding Challenge #2
+// Set
 
-/* 
-Let's continue with our football betting app!
+// const ordersSet = new Set(["Pasta", "Pizza", "Pizza", "Risotto", "Pasta", "Pizza"]);
+// console.log(ordersSet);
 
-1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
-2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
-3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
-      Odd of victory Bayern Munich: 1.33
-      Odd of draw: 3.25
-      Odd of victory Borrussia Dortmund: 6.5
-Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+// console.log(new Set("Jonas"));
 
-BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
-      {
-        Gnarby: 1,
-        Hummels: 1,
-        Lewandowski: 2
-      }
+// const person = new Set({ name: "ali", age: 15 });
+// console.log(person);
 
-GOOD LUCK 😀
-*/
+// console.log(ordersSet.size);
+// console.log(ordersSet.has("Pizza"));
+// console.log(ordersSet.has("Bread"));
+// ordersSet.add("Garlic Bread");
+// ordersSet.add("Garlic Bread");
+// ordersSet.delete("Risotto");
+// // ordersSet.clear();
+// console.log(ordersSet);
 
-const game = {
-  team1: "Bayern Munich",
-  team2: "Borrussia Dortmund",
-  players: [
-    [
-      "Neuer",
-      "Pavard",
-      "Martinez",
-      "Alaba",
-      "Davies",
-      "Kimmich",
-      "Goretzka",
-      "Coman",
-      "Muller",
-      "Gnarby",
-      "Lewandowski",
-    ],
-    [
-      "Burki",
-      "Schulz",
-      "Hummels",
-      "Akanji",
-      "Hakimi",
-      "Weigl",
-      "Witsel",
-      "Hazard",
-      "Brandt",
-      "Sancho",
-      "Gotze",
-    ],
-  ],
-  score: "4:0",
-  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
-  date: "Nov 9th, 2037",
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// for (const order of ordersSet) console.log(order);
 
-// 1.
-const players = game.scored;
+// Example
+const staff = ["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
 
-for (const [i, player] of players.entries()) {
-  console.log(`Goal ${i + 1}: ${player}`);
-}
+console.log(new Set(staff).size);
 
-// for (let i = 0; i < players.length; i++) {
-//   console.log(`Goal ${i + 1}: ${players[i]}`);
-// }
-
-/**********************************************/
-// 2.
-
-const values = Object.values(game.odds);
-
-let odds = 0;
-for (const odd of values) {
-  odds += odd;
-}
-console.log(odds / values.length);
-
-/**********************************************/
-// 3.
-
-const entries = Object.entries(game.odds);
-
-for (const [teamName, score] of entries) {
-  console.log(teamName);
-  // const teamStr = teamName === "x" ? "draw" : `victory ${game[teamName]}`;
-  // console.log(`Odd of ${teamStr}: ${score}`);
-}
-
-// BONUS: Create an object called 'scorers' which contains the names of the players who scored, as properties, and the number of goals as the value. In this game, it will look like this:
-//       {
-//         Gnarby: 1,
-//         Hummels: 1,
-//         Lewandowski: 2
-//       }
-
-/**********************************************/
-// bonus.
-
-// So the solution is to loop over the array, and add the array elements as object properties, and then increase the count as we encounter a new occurence of a certain element
-
-const scorers = {};
-
-for (const player of game.scored) {
-  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
-}
-
-console.log(scorers);
+console.log(new Set("jonasschmedtmann").size);
