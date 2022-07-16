84955b741f3091ea0794246ce57c6f23ab93af9d
shokouhSkr <shokouhskr@gmail.com>
2022-07-16 17:32:29 +0430

    Map - iteration

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
 "use strict";
 
-// Map - fundamental
+// Map - iteration
 
-const rest = new Map();
-rest.set("name", "Classico Italiano");
-rest.set(1, "Firenze, Italy");
-rest.set(2, "Lisbon, Portugal");
-console.log(rest);
+const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 
-rest
-  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
-  .set("open", 11)
-  .set("close", 23)
-  .set(true, "We are open :D")
-  .set(false, "We are closed :(");
+const question = new Map([
+  ["question", "What is the best programming language in the world?"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["correct", 3],
+  [true, "Correct 🎉"],
+  [false, "Try again!"],
+]);
+console.log(question);
 
-// console.log(rest.get("name"));
-// console.log(rest.get("open"));
-// console.log(rest.get(true));
-// console.log(rest.get(1));
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
 
-// const time = 8;
-// console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+// Quiz app
+console.log(question.get("question"));
+console.log(question.get(true));
+for (const [key, value] of question) {
+  if (typeof key === "number") console.log(`Answer ${key}: ${value}`);
+}
 
-// console.log(rest.has("categories"));
-// rest.delete(2);
-// // rest.clear();
+const answer = Number(prompt("Your answer"));
+console.log(answer);
+console.log(question.get(question.get("correct") === answer));
 
-const arr = [1, 2];
-rest.set(arr, "Test");
-rest.set(document.querySelector("h1"), "Heading");
-console.log(rest);
-console.log(rest.size);
-
-console.log(rest.get(arr));
+// Convert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
