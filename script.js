35a6624b6af786eec0d480dc0450ad9626d98ec8
shokouhSkr <shokouhskr@gmail.com>
2022-07-18 23:02:47 +0430

    How Passing Arguments Works

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 "use strict";
 
-// Default Parameters
+// How Passing Arguments Works: Values vs. Reference
 
-const bookings = [];
-
-const createBooking = function (flightNum, numPassengers = 1, price = 199 * numPassengers) {
-  // ES5
-  // numPassengers = numPassengers || 1;
-  // price = price || 199;
-
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+const flight = "LH234";
+const jonas = {
+  name: "Jonas Schmedtmann",
+  passport: 24739479284,
 };
 
-createBooking("LH123");
-createBooking("LH123", 2, 800);
-createBooking("LH123", 2);
-createBooking("LH123", 5);
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH999";
+  passenger.name = "Mr. " + passenger.name;
 
-createBooking("LH123", undefined, 1000);
+  if (passenger.passport === 24739479284) {
+    alert("Checked in");
+  } else {
+    alert("Wrong passport!");
+  }
+};
 
-console.log(bookings);
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
