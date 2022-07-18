fc2093d4f1dfcc2d5bb4939308fc6aa444d167ae
shokouhSkr <shokouhskr@gmail.com>
2022-07-18 22:54:21 +0430

    Default Parameters

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 "use strict";
 
-// String method practice
+// Default Parameters
 
-const flights =
-  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+const bookings = [];
 
-// 🔴 Delayed Departure from FAO to TXL (11h25)
-//              Arrival from BRU to FAO (11h45)
-//   🔴 Delayed Arrival from HEL to FAO (12h05)
-//            Departure from FAO to LIS (12h30)
+const createBooking = function (flightNum, numPassengers = 1, price = 199 * numPassengers) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
 
-// const arr = flights.split("+");
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
 
-const getCode = (str) => str.slice(0, 3).toUpperCase();
+createBooking("LH123");
+createBooking("LH123", 2, 800);
+createBooking("LH123", 2);
+createBooking("LH123", 5);
 
-for (const flight of flights.split("+")) {
-  const [type, from, to, time] = flight.split(";");
-  const output = `${type.startsWith("_Delayed") ? "🔴" : ""}${type.replaceAll(
-    "_",
-    " "
-  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(":", "h")})`.padStart(45);
+createBooking("LH123", undefined, 1000);
 
-  console.log(output);
-}
+console.log(bookings);
