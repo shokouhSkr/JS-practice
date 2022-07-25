aa85269caa49dfb31a0dc6c6c627688a0850561d
shokouhSkr <shokouhskr@gmail.com>
2022-07-25 17:56:36 +0430

    The new at Method

--- script.js
+++ script.js
@@ -1,35 +1,15 @@
 "use strict";
 
-// Simple Array Methods
+// The new at Method
 
-let arr = ["a", "b", "c", "d", "e"];
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
 
-// SLICE
-console.log(arr.slice(2));
-console.log(arr.slice(2, 4));
-console.log(arr.slice(-2));
-console.log(arr.slice(-1));
-console.log(arr.slice(1, -2));
-console.log(arr.slice());
-console.log([...arr]);
+// getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
 
-// SPLICE
-// console.log(arr.splice(2));
-arr.splice(-1);
-console.log(arr);
-arr.splice(1, 2);
-console.log(arr);
-
-// REVERSE
-arr = ["a", "b", "c", "d", "e"];
-const arr2 = ["j", "i", "h", "g", "f"];
-console.log(arr2.reverse());
-console.log(arr2);
-
-// CONCAT
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
-
-// JOIN
-console.log(letters.join(" - "));
+console.log("jonas".at(0));
+console.log("jonas".at(-1));
