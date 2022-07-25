751cd0734d2be9d889d44b49625149f2ed1409c2
shokouhSkr <shokouhskr@gmail.com>
2022-07-25 17:17:46 +0430

    simple array methods

--- script.js
+++ script.js
@@ -1,63 +1,35 @@
 "use strict";
 
-// Closures
-const secureBooking = function () {
-  let passengerCount = 0;
+// Simple Array Methods
 
-  return function () {
-    passengerCount++;
-    console.log(`${passengerCount} ${passengerCount === 1 ? "passenger" : "passengers"}`);
-  };
-};
+let arr = ["a", "b", "c", "d", "e"];
 
-const booker = secureBooking();
+// SLICE
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
 
-booker();
-booker();
-booker();
+// SPLICE
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
 
-console.dir(booker);
+// REVERSE
+arr = ["a", "b", "c", "d", "e"];
+const arr2 = ["j", "i", "h", "g", "f"];
+console.log(arr2.reverse());
+console.log(arr2);
 
-///////////////////////////////////////
-// More Closure Examples
-// Example 1
-let f;
+// CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
 
-const g = function () {
-  const a = 23;
-  f = function () {
-    console.log(a * 2);
-  };
-};
-
-const h = function () {
-  const b = 777;
-  f = function () {
-    console.log(b * 2);
-  };
-};
-
-g();
-f();
-console.dir(f);
-
-// Re-assigning f function
-h();
-f();
-console.dir(f);
-
-// Example 2
-const boardPassengers = function (n, wait) {
-  const perGroup = n / 3;
-
-  setTimeout(function () {
-    console.log(`We are now boarding all ${n} passengers`);
-    console.log(`There are 3 groups, each with ${perGroup} passengers`);
-  }, wait * 1000);
-
-  console.log(`Will start boarding in ${wait} seconds`);
-};
-
-const perGroup = 1000;
-boardPassengers(180, 3);
-*/
+// JOIN
+console.log(letters.join(" - "));
