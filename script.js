c0a122d2c0433a27fb9bcf564c6b97ced1adda0d
shokouhSkr <shokouhskr@gmail.com>
2022-07-31 22:36:42 +0430

    The map method

--- script.js
+++ script.js
@@ -1,41 +1,24 @@
 "use strict";
 
-// Coding Challenge #1
+// The map Method
 
-/* 
-Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1;
 
-Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
 
-1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
-2. Create an array with both Julia's (corrected) and Kate's data
-3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
-4. Run the function for both test datasets
+const movementsUSD = movements.map((mov) => mov * eurToUsd);
 
-HINT: Use tools from all lectures in this section so far 😉
+console.log(movements);
+console.log(movementsUSD);
 
-TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
-TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
 
-GOOD LUCK 😀
-*/
-
-const checkDogs = function (dogsJulia, dogsKate) {
-  const copyDogsJulia = [...dogsJulia];
-  const onlyDogsJulia = copyDogsJulia.slice(1, copyDogsJulia.length - 2);
-
-  const bothDogs = [...onlyDogsJulia, ...dogsKate];
-  console.log(bothDogs);
-
-  bothDogs.forEach(function (dogAge, i) {
-    if (dogAge < 3) {
-      console.log(`Dog number ${i + 1} is still a puppy 🐶`);
-    } else {
-      console.log(`Dog number ${i + 1} is an adult, and is ${dogAge} years old"`);
-    }
-  });
-};
-
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
-console.log("-------------------- Another data -------------------");
-checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+const movementsDescriptions = movements.map(
+  (mov, i) => `Movement ${i + 1}: You ${mov > 0 ? "deposited" : "withdrew"} ${Math.abs(mov)}`
+);
+console.log(movementsDescriptions);
